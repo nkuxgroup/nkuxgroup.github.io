6682debadf15206684e325320ad4d7f8ef793952
Alex <davis.alex.jr@gmail.com>
2018-05-22 12:55:51 -0400

    Added about section and events page

---
+++ events.php
@@ -0,0 +1,9 @@
+<?php include 'header.php';?>
+
+<div class="events hero">
+    <div class="hero-container text-center">
+        <h1>EVENTS</h1>
+    </div>
+</div>
+
+<?php include 'footer.php';?>
--- home.php
+++ home.php
@@ -11,4 +11,28 @@
     </div>
 </div>
 
+<div id="about">
+    <div class="container">
+        <h1 class="text-center mb-5">What is UX?</h1>
+        <p>
+            Lorizzle ipsum dolizzle sit my shizz, fizzle adipiscing dawg. Nullam sapien velizzle, aliquet volutpizzle, suscipit quizzle, dawg vel, arcu. Pellentesque doggy crackalackin. Uhuh ... yih! erizzle. Bizzle izzle brizzle dapibizzle turpis tempus check out this. Maurizzle pellentesque mah nizzle izzle crunk. Vestibulum izzle tortor
+        </p>
+        <div class="ux-demo mt-5">
+            <div class="ux-demo-item">
+                <button class="btn btn-black">CLICK ME</button>
+                <p>I was clicked <b>20</b> times!</p>
+            </div>
+            <div class="ux-demo-item">
+                <button class="btn btn-red">CLICK ME</button>
+                <p>I was clicked <b>23</b> times!</p>
+            </div>
+            <div class="ux-demo-item">
+                <button class="btn btn-blue">CLICK ME</button>
+                <p>I was clicked <b>19</b> times!</p>
+            </div>
+            <p>The <b class="color-red">red button</b> has the most interaction!</p>
+        </div>
+    </div>
+</div>
+
 <?php include 'footer.php';?>
