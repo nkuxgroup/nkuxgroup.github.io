83896522109fe25c7de1edc9ac645c3c87e17c31
Alex <davis.alex.jr@gmail.com>
2018-05-22 12:26:14 -0400

    Made changes so nav is always fixed. Added home hero section.

--- home.php
+++ home.php
@@ -1,6 +1,14 @@
 <?php include 'header.php';?>
 
 <div class="home hero">
+    <div class="hero-container text-center">
+        <h1>NKUX</h1>
+        <h2>The NKU User Experience Group</h2>
+        <div class="d-flex flex-sm-row flex-column justify-content-center mt-2">
+            <a href="#about" class="btn btn-outline m-2">LEARN MORE</a>
+            <a href="events.php" class="btn btn-black m-2">JOIN US</a>
+        </div>
+    </div>
 </div>
 
 <?php include 'footer.php';?>
