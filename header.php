176d55477c13f8d6ec4e9373ebcb251c0d9d34a5
Alex <davis.alex.jr@gmail.com>
2018-05-22 10:55:10 -0400

    Added pages and sass

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/master.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+	<title>NKUX</title>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<div class="container">
+		<a class="navbar-brand" href="#">NKUX</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+			aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link" href="#">ABOUT</a>
+				<a class="nav-item nav-link" href="#">EVENTS</a>
+				<a class="nav-item nav-link" href="#">CONTACT</a>
+			</div>
+		</div>
+	</div>
+</nav>
---
+++ home.php
@@ -0,0 +1,6 @@
+<?php include 'header.php';?>
+
+<div class="home hero">
+</div>
+
+<?php include 'footer.php';?>
